493556ec96de15f319d98fd033627df3d3b523eb
Andrei Zavgorodnii <andrei_zavgorodnii@sonm.io>
2017-12-06 19:54:50 +0300

    fixes for formulas

--- funcs.py
+++ funcs.py
@@ -3,7 +3,10 @@
 import math
 
 
-def baseline(history, memory=0.9):
+import numpy
+
+
+def get_reputation(history, memory=0.9):
     h_len = len(history)
 
     if h_len < 1:
@@ -19,3 +22,19 @@ def baseline(history, memory=0.9):
             neg += math.fabs(history[idx]) * math.pow(memory, h_len - idx)
 
     return (pos + 1.0) / (pos + neg + 5.)
+
+
+def get_max_deal_price(reputation, history, start_mu=2.):
+    if len(history) < 1:
+        return reputation * start_mu
+
+    return reputation * numpy.mean(history) + 0.2 * numpy.mean(history)
+
+
+def propose_deal():
+    price = numpy.random.normal(15, 30, 1)
+
+    if price[0] < 0:
+        return 5.
+
+    return price[0]
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import funcs
 import histories
 
 
-def plot_reputation(history, func):
-    y_axis = [func(history[:x], memory=0.99) for x in xrange(len(history))]
+def plot_reputation(history, reputation_func):
+    y_axis = [reputation_func(history[:x], memory=0.99) for x in xrange(len(history))]
     x_axis = [x for x in xrange(len(history))]
 
     plt.plot(x_axis, y_axis, 'ro')
@@ -21,11 +21,42 @@ def plot_reputation(history, func):
     plt.show()
 
 
+def plot_max_deal_cost(num_tries, reputation_func, max_price_func):
+    history = []
+    y_axis = []
+    for x in xrange(num_tries):
+        rep = reputation_func(history[:x], memory=0.99)
+        price = funcs.propose_deal()
+        max_price = max_price_func(rep, history[:x])
+        y_axis.append(max_price)
+
+        print("rep:", rep, "max price:", max_price, "proposed price:", price)
+
+        if price <= max_price:
+            history.append(price)
+
+    # x_axis = [x for x in xrange(num_tries)]
+    #
+    # plt.plot(x_axis, y_axis, 'ro')
+    #
+    # ax = plt.gca()
+    # # recompute the ax.dataLim
+    # ax.relim()
+    # # update ax.viewLim using the new dataLim
+    # ax.autoscale_view()
+    #
+    # plt.show()
+
+
 def main():
     # plot_reputation(histories.uniform_ideal(100), funcs.baseline)
-    # plot_reputation(histories.uniform_ideal_restored(), funcs.baseline)
+    # plot_max_deal_cost(200, funcs.get_reputation, funcs.get_max_deal_price)
+
+    plot_reputation(histories.uniform_ideal_restored(), funcs.get_reputation)
     # plot_reputation(histories.uniform_ideal_restored(ideal_count=30, fuckup_count=6, restore_count=30), funcs.baseline)
-    plot_reputation(histories.uniform_ideal(100) + histories.uniform_oscillate(100), funcs.baseline)
+    # plot_reputation(histories.uniform_ideal(100) + histories.uniform_oscillate(100), funcs.reputation)
+
+
 
 
 if __name__ == "__main__":
