03d631187b9826791bde06df172ed4b4a6f22bcb
DoctorCotic <highfsociety@gmail.com>
2017-12-15 01:41:25 +0300

    checkout func.prog,Fraud(price)

--- funcs.py
+++ funcs.py
@@ -1,6 +1,9 @@
 import math
 
-def baseline(history, memory=0.9):
+import numpy
+
+
+def get_reputation(history, memory=0.9, weight=0.3):
     if not history:
         return 0.5
     h_len = len(history)
@@ -14,11 +17,20 @@ def baseline(history, memory=0.9):
         else:
             neg += math.fabs(item) * math.pow(memory, h_len - idx)
         idx += 1
-    return (pos + 1.0) / (pos + neg + 5.)
 
-    # for idx in range(h_len):
-    #     if history[idx] > 0:
-    #         pos += history[idx] * math.pow(memory, h_len - idx)
-    #     else:
-    #         neg += math.fabs(history[idx]) * math.pow(memory, h_len - idx)
-    # return (pos + 1.0) / (pos + neg + 5.)
+    stabilizer = numpy.mean(history)
+    return (weight * pos + stabilizer) / ((weight * pos) + neg + 2. + stabilizer)
+
+
+def get_max_deal_price(reputation, history, market_mu=100.0, trusted_history=5):
+    if len(history) < trusted_history:
+        return reputation * market_mu
+    return reputation * sum(history)
+
+
+# цены на рынке
+def get_proposed_price(mu, std):
+    price = numpy.random.normal(mu, std, 1)
+    if price[0] < 0:
+        return 1.
+    return price[0]
--- main.py
+++ main.py
@@ -1,15 +1,61 @@
+import numpy
+import matplotlib.pyplot as plt
 import histories
+import funcs
+
+
+def can_make_deal(price, history, market_mean_price):
+    # возвращает ценник между mu+2sigma <= X <= mu-2sigma
+    if len(history) == 0:
+        mean = market_mean_price
+        std = mean * 1.41
+    else:
+        mean = numpy.mean(history)
+        std = numpy.std(history)
+        if std == 0:
+            std = mean * 1.41
+
+    lower = mean - 2 * std
+    upper = mean + 2 * std
+    return lower < price < upper
+
 
 def main():
-    s1 = histories.Deals(500)
-    s2 = histories.Deals(500, 0.1, 100, 0)
-    s3 = histories.Deals(500, 0.05, 5, 100)
-    s4 = histories.Deals(1000, 0.1)
+    history = histories.uniform_prices(120) + histories.uniform_with_conflicts(20, each=3)
+    # history = histories.uniform_prices(120) + histories.uniform_fuckup(30)
+    history_without_outliers = []
+    market_mean_price = numpy.mean(history)
 
-    qq = histories.Deals()
-    qq = qq.fuck(10).ok(13).fuck(5).ok(52).fuck(10).fuck(10)
-    print(len(histories.Deals.fuckrow))
-    qq.plot()
+    for x in range(len(history)):
+        if len(history_without_outliers) < 12:
+            print("history without outliers:", history_without_outliers)
+            hist = history[:12]
+        else:
+            hist = history_without_outliers[:x]
+
+        if can_make_deal(numpy.fabs(history[x]), hist, market_mean_price):
+            history_without_outliers.append(history[x])
+        else:
+            print(x)
+            history_without_outliers.append(history_without_outliers[-1])
+
+    y_axis = [funcs.get_reputation(history_without_outliers[:x], memory=0.99)
+              for x in range(len(history_without_outliers))]
+    x_axis = [x for x in range(len(history_without_outliers))]
+
+    plt.plot(x_axis, y_axis, 'ro',
+             label='Rating(Q)',
+             linewidth=2,
+             linestyle='dashed',
+             marker='o',
+             markersize=2,
+             markerfacecolor='blue')
+    plt.ylabel('$Fraud (price)$')
+    plt.xlabel('$Q$')
+    plt.title(u"Dependence f(x) on the moment in the history of deals")
+    plt.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
